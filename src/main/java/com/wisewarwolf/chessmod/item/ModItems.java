cbe5991fecde3e70fbc2dd5da4d39433af81482c
FlamingWolf <46968587+FlamingWolf@users.noreply.github.com>
2023-01-12 21:14:19 -0600

    Items class

---
+++ src/main/java/com/wisewarwolf/chessmod/item/ModItems.java
@@ -0,0 +1,31 @@
+package com.wisewarwolf.chessmod.item;
+
+import com.wisewarwolf.chessmod.ChessMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ChessMod.MOD_ID);
+
+    public static final RegistryObject<Item> WHITE_PAWN = ITEMS.register("white_pawn",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> WHITE_QUEEN = ITEMS.register("white_queen",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> BLACK_QUEEN = ITEMS.register("black_queen",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> BLACK_PAWN = ITEMS.register("black_pawn",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
